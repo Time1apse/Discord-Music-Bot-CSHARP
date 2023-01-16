755c5753ce411c1ac302ee0904dc7cd5c5ded117
larion <larion@BTM-R507-C14>
2023-01-16 16:11:31 +0300

    Добавьте файлы проекта.

---
+++ Discord Music Bot CSHARP/Core/Bot.cs
@@ -0,0 +1,34 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discord_Music_Bot_CSHARP.Core;
+
+public class Bot {
+
+    private Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+
+    private DiscordSocketClient _client;
+
+    public async Task MainAsync()
+    {
+        _client = new DiscordSocketClient();
+        _client.Log += Log;
+
+        var token = "ODgxMzk0ODA1MjQ5Mjk4NDYy.GMKVMc.zAJlJ8pqI50hP8xhR73sCP3Lr_zVyF0Po0kOkw";
+
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+
+        await Task.Delay(-1);
+    }
+       
+}
---
+++ Discord Music Bot CSHARP/Program.cs
@@ -0,0 +1,4 @@
+﻿namespace Discord_Music_Bot_CSHARP.Core;
+public class Program {
+    public static async Task Main(string[] args) => new Bot().MainAsync().GetAwaiter().GetResult();
+}
